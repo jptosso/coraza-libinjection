42cad9390da94e67049c13f8ff342b605f167631
Juan Pablo Tosso <jptosso@gmail.com>
2021-11-14 23:44:03 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/jptosso/coraza-libinjection
+
+go 1.17
+
+require github.com/jptosso/coraza-waf/v2 v2.0.0-alpha.1
+
+require (
+	github.com/cloudflare/ahocorasick v0.0.0-20210425175752-730270c3e184 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.19.1 // indirect
+)
---
+++ plugin.go
@@ -0,0 +1,54 @@
+// Copyright 2021 Juan Pablo Tosso
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package plugin
+
+import (
+	"github.com/jptosso/coraza-waf/v2"
+	"github.com/jptosso/coraza-waf/v2/operators"
+)
+
+type sqli struct {
+}
+
+func (o *sqli) Init(data string) error {
+	return nil
+}
+
+func (o *sqli) Evaluate(tx *coraza.Transaction, value string) bool {
+	res, capture := isSQLi(value)
+	tx.CaptureField(1, capture)
+	return res
+}
+
+type xss struct {
+}
+
+func (o *xss) Init(data string) error {
+	return nil
+}
+
+func (o *xss) Evaluate(tx *coraza.Transaction, value string) bool {
+	return isXSS(value)
+}
+
+func init() {
+	operators.RegisterOperator("detectSQLi", func() coraza.RuleOperator { return new(sqli) })
+	operators.RegisterOperator("detectXSS", func() coraza.RuleOperator { return new(xss) })
+}
+
+var (
+	_ coraza.RuleOperator = &sqli{}
+	_ coraza.RuleOperator = &xss{}
+)
